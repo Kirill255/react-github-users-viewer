2d54c208dac3b5fe17164b8e9c3e2ba12ff0deed
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-04-05 20:39:59 +0300

    Updates

---
+++ precache-manifest.48bbc3d7b87b033b49913783e34bd7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69d38327c8ae7429582c",
+    "url": "/static/css/main.f896913d.chunk.css"
+  },
+  {
+    "revision": "69d38327c8ae7429582c",
+    "url": "/static/js/main.bc7ea671.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e607659ecd13fc7ff277",
+    "url": "/static/css/2.bb4303df.chunk.css"
+  },
+  {
+    "revision": "e607659ecd13fc7ff277",
+    "url": "/static/js/2.d4303f82.chunk.js"
+  },
+  {
+    "revision": "3a211e31224e94698332a0ff45dc1a5d",
+    "url": "/index.html"
+  }
+];
